e2cb1391f82ac72284e54e2e04840ec0618d5d1b
21isenough <39227503+21isenough@users.noreply.github.com>
2019-11-23 19:11:32 +0000

    add random number

--- 11.0/ruben/ln_cashback/models/models.py
+++ 11.0/ruben/ln_cashback/models/models.py
@@ -15,17 +15,27 @@ class LnCashback(models.Model):
     pos_statement_id = fields.Many2one(
         "pos.order", string="POS statement", ondelete="cascade"
     )
-    ln_status = fields.Selection([
-        ('PENDING', 'Pending LN Payment'),
-        ('CLAIMED', 'Claimed LN Payment'),
-        ('EXPIRED', 'Expired LN Payment'),
-        ('CANCELLED', 'Cancelled LN Payment'),
-    ], help="""
+    ln_status = fields.Selection(
+        [
+            ("PENDING", "Pending LN Payment"),
+            ("CLAIMED", "Claimed LN Payment"),
+            ("EXPIRED", "Expired LN Payment"),
+            ("CANCELLED", "Cancelled LN Payment"),
+        ],
+        help="""
 * Pending LN Payment -- LNURL that still needs to be claimed by customer
 * Claimed LN Payment -- LNURL claimed by customer
 * Expired LN Payment -- Customer did not claim LNURL
 * Cancelled LN Payment -- We cancelled the LNURL
-    """)
+    """,
+    )
+
+    randomnum = fields.Char(compute="_compute_rand")
+
+    @api.multi
+    def _compute_rand(self):
+        for record in self:
+            record.randomnum = str(random.randint(1, 1e6))
 
     @api.multi
     def add_sats(self):
