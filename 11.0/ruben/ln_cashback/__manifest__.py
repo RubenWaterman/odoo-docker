5443490ce3acb3cc373595f0a2477f5d06b60925
Ruben Waterman <rubenwaterman@me.com>
2019-11-23 19:08:31 +0000

    update dependencies

--- 11.0/ruben/ln_cashback/__manifest__.py
+++ 11.0/ruben/ln_cashback/__manifest__.py
@@ -7,15 +7,15 @@
     "description": """
         Long description of module's purpose
     """,
-    "author": "My Company",
-    "website": "http://www.yourcompany.com",
+    "author": "@21isenough",
+    "website": "https://www.twitter.com/21isenough",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
     # for the full list
     "category": "Uncategorized",
     "version": "0.1",
     # any module necessary for this one to work correctly
-    "depends": ["base"],
+    "depends": ["base","point_of_sale"],
     # always loaded
     "data": [
         # 'security/ir.model.access.csv',
