6c6ca23d590bc7f3f93df60a41d1d47115adcecd
21isenough <39227503+21isenough@users.noreply.github.com>
2019-11-23 13:52:23 +0000

    add authors

--- 11.0/ruben/ln_cashback/__manifest__.py
+++ 11.0/ruben/ln_cashback/__manifest__.py
@@ -1,35 +1,27 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "ln_cashback/",
-
-    'summary': """
+    "name": "ln_cashback/",
+    "summary": """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
+    "description": """
         Long description of module's purpose
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
+    "author": "@watermaniak, @21isenough",
+    "website": "http://www.yourcompany.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
+    "category": "Uncategorized",
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
+    "depends": ["base"],
     # always loaded
-    'data': [
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        "views/views.xml",
+        "views/templates.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    "demo": ["demo/demo.xml",],
 }
